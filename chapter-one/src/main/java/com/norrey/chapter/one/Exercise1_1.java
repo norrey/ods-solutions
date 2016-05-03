a26b14bf9be83bec4b1acd5d9d13e4c9886f5f0b
Norrey Okumu <okumu.norrey@gmail.com>
2016-05-03 09:02:48 +0300

    Added part two of exercise 1.1

--- chapter-one/src/main/java/com/norrey/chapter/one/Exercise1_1.java
+++ chapter-one/src/main/java/com/norrey/chapter/one/Exercise1_1.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
@@ -12,6 +13,7 @@ import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 
 import static java.util.Objects.requireNonNull;
+import java.util.Scanner;
 
 /**
  *
@@ -38,6 +40,28 @@ public class Exercise1_1 {
 
         Collections.reverse(lineList);
         lineList.stream().forEach(System.out::println);
+    }
+
+    /**
+     *
+     * @param file
+     * @throws IOException
+     */
+    public void partTwo(@Nonnull final File file) throws IOException {
+        final List<String> lineList = ImmutableList.of();
+        int lineNumber = 0;
+        final Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            lineList.add(scanner.nextLine());
+            if (lineNumber == 49) {
+                Collections.reverse(lineList);
+                lineList.stream().forEach(System.out::println);
+                lineList.clear();
+                lineNumber = 0;
+            }
+            lineNumber++;
+        }
+
     }
 
 }
